dd620f0b33437c1aa0c1d4db95c99f676b2c3204
Lax Hsieh <laxative0612@gmail.com>
2025-04-07 20:31:44 +0800

    Update Q51 algorithm

--- Q51-solveNQueens.js
+++ Q51-solveNQueens.js
@@ -5,10 +5,10 @@
 var solveNQueens = function(n) {
   const ans = [];
 
-  // pos: [x, y]
+  // pos: [y, x]
   const checkPosIsValid = (curQueenPos, tarQueenPos) => {
-      if (curQueenPos[0] === tarQueenPos[0]) return false; // same col
-      if (curQueenPos[1] === tarQueenPos[1]) return false; // same row
+      if (curQueenPos[0] === tarQueenPos[0]) return false; // same row
+      if (curQueenPos[1] === tarQueenPos[1]) return false; // same col
 
       if (
           Math.abs(tarQueenPos[0] - curQueenPos[0]) === Math.abs(tarQueenPos[1] - curQueenPos[1])
@@ -36,7 +36,7 @@ var solveNQueens = function(n) {
       return puzzle;
   };
 
-  // pos: [x, y]
+  // pos: [y, x]
   const finder = (currQueensPos, targetPos) => {
       const isValid = currQueensPos.every((currPos) => checkPosIsValid(currPos, targetPos));
       if (isValid) {
@@ -44,16 +44,20 @@ var solveNQueens = function(n) {
           if (newCurrQueensPos.length === n) {
               ans.push(transferToPuzzle(newCurrQueensPos));
           } else {
-              for (let i = targetPos[0] * n + targetPos[1] + 1; i < n * n; i++) {
-                  finder(newCurrQueensPos, [Math.floor(i / n), i % n]);
+              // if is already last row, directly return(not found)
+              if (targetPos[0] === n - 1) return;
+
+              // directly find next row
+              for (let i = 0; i < n; i++) {
+                  finder(newCurrQueensPos, [targetPos[0] + 1, i]);
               }
           }
       }
   };
 
   for (let i = 0; i < n; i++) {
-      finder([], [0, i % n]);
+      finder([], [0, i]);
   }
-  
+
   return ans;
 };
