3f248b483d4bd91acaa2586f67556fb873eda63d
Lax Hsieh <laxative0612@gmail.com>
2025-04-11 22:31:35 +0800

    Update Q151 code, not use js origin function

--- Q151-reverseWords.js
+++ Q151-reverseWords.js
@@ -3,10 +3,34 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    let words = [];
-    s.split(' ').forEach((word) => {
-        if (word !== '') words.unshift(word);
-    });
+    let currWord = '';
+    let ans = '';
 
-    return words.join(' ');
+    const reverse = (word) => {
+        let output = '';
+        for (let i = word.length - 1; i >= 0; i--) output += word.charAt(i);
+
+        return output;
+    };
+
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (s.charAt(i) === ' ') {
+            if (currWord !== '') {
+                const reverseCurrWord = reverse(currWord);
+                currWord = '';
+
+                if (ans !== '') ans += ' ';
+                ans += reverseCurrWord;
+            }
+        } else {
+            currWord += s.charAt(i);
+        }
+    }
+
+    if (currWord !== '') {
+        if (ans !== '') ans += ' ';
+        ans += reverse(currWord);
+    }
+
+    return ans;
 };
